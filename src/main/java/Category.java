337a64010174629bfae7e137aceb822e29117efa
LucasKendy1 <87815996+LucasKendy1@users.noreply.github.com>
2024-03-28 20:01:05 -0300

    Primeiro commit

---
+++ src/main/java/Category.java
@@ -0,0 +1,27 @@
+public enum Category {
+
+        TERROR,
+        ROMANCE,
+        COMEDIA,
+        COMEDIA_ROMANTICA,
+        SUSPENSE,
+        FICCAO_CIENTIFICA,
+        ACAO,
+        DRAMA,
+        ARTIGO,
+        EBOOK,
+        FANTASIA,
+        CONTOS_DE_FADA,
+        AVENTURA,
+        INFANTIL,
+        TECNOLOGIA,
+        RELIGIAO,
+        GASTRONOMIA,
+        CYBERPUNK,
+        HISTORIA,
+        FOTOGRAFIA,
+        BIOGRAFIA,
+        SAUDE
+
+
+}
---
+++ src/test/java/com/kendy/bookManagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kendy.bookManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

26e3379e23a411495d8012d8cb5188a881757773
LucasKendy1 <87815996+LucasKendy1@users.noreply.github.com>
2024-03-30 17:39:27 -0300

    Post function added successfully

--- src/main/java/com/kendy/bookManagement/controllers/BookController.java
+++ src/main/java/com/kendy/bookManagement/controllers/BookController.java
@@ -1,14 +1,17 @@
 package com.kendy.bookManagement.controllers;
 
 import com.kendy.bookManagement.models.Book;
+import com.kendy.bookManagement.models.BookDto;
 import com.kendy.bookManagement.services.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -20,4 +23,15 @@ public class BookController {
     public List<Book> getAllBooks() {
         return repo.findAll();
     }
+
+    @PostMapping("/create")
+    public String createBook(
+             @ModelAttribute BookDto bookDto,
+            BindingResult result
+    ){
+        return "Book created";
+    }
+
+
+
 }
